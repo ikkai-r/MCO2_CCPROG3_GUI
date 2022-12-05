328064a7527f4e8620db6ab29a95c4bf0119e8c2
ikkai-r <rm.sales92@gmail.com>
2022-12-05 08:59:24 +0800

    Finished title screen. Need button functionality.

--- src/gui/GUI.java
+++ src/gui/GUI.java
@@ -1,9 +1,15 @@
 package gui;
 
 import gui.scenes.SceneButtons;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 public class GUI {
@@ -29,9 +35,30 @@ public class GUI {
 
     public void openingSceneButtons() {
         mainPane.setId("pane");
-        SceneButtons startButton = new SceneButtons("Start");
-        mainPane.getChildren().add(startButton);
+        VBox box = openingSceneVBOX();
+        mainPane.getChildren().add(box);
+        box.setAlignment(Pos.CENTER);
+        box.prefWidthProperty().bind(mainPane.widthProperty());
+        box.prefHeightProperty().bind(mainPane.heightProperty());
         mainScene.getStylesheets().add(this.getClass().getResource("/style.css").toExternalForm());
+    }
+
+    public VBox openingSceneVBOX() {
+        Font.loadFont(this.getClass().getResource("/Stardew_Valley.ttf").toExternalForm(), 20);
+
+        SceneButtons startButton = new SceneButtons("Start");
+        SceneButtons exitButton = new SceneButtons("Exit");
+        HBox hbox = new HBox(40);
+        hbox.getChildren().addAll(startButton, exitButton);
+        hbox.setAlignment(Pos.CENTER);
+        Image image = new Image("title_screen.png");
+        ImageView imageView = new ImageView();
+        //Setting image to the image view
+        imageView.setImage(image);
+        //Setting the image view parameters
+        imageView.setFitWidth(750);
+        imageView.setPreserveRatio(true);
+        return new VBox(50, imageView, hbox);
     }
 
 }
--- src/gui/scenes/SceneButtons.java
+++ src/gui/scenes/SceneButtons.java
@@ -2,34 +2,33 @@ package gui.scenes;
 
 import javafx.event.EventHandler;
 import javafx.scene.control.Button;
+import javafx.scene.effect.BlurType;
 import javafx.scene.effect.DropShadow;
 import javafx.scene.input.MouseButton;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+
+import java.io.FileInputStream;
 
 public class SceneButtons extends Button {
 
     public SceneButtons(String text) {
         setText(text);
-        setPrefWidth(180);
-        setPrefHeight(49);
-        setTranslateX(200);
-        setTranslateY(25);
-        setButtonStyleUnpressed();
         initButtonListeners();
+        setPrefWidth(190);
+        setPrefHeight(70);
+        setButtonStyleUnpressed();
     }
 
     public void setButtonStyleUnpressed() {
-        setStyle("-fx-background-color: transparent; -fx-background-image: url('button_pressed.png'); -fx-background-repeat: stretch; -fx-background-size: 180 45;");
-        setPrefWidth(180);
-        setPrefHeight(45);
-        setLayoutY(getLayoutY()+4);
+        setStyle("-fx-background-color: transparent; -fx-background-image: url('button_pressed.png'); -fx-background-repeat: stretch; -fx-background-size: 190 70;");
+        setPrefWidth(190);
+        setPrefHeight(70);
     }
 
     public void setButtonStylePressed() {
-        setStyle("-fx-background-color: transparent; -fx-background-image: url('button_pressed.png'); -fx-background-repeat: stretch; -fx-background-size: 180 45;");
-        setPrefHeight(49);
-        setLayoutY(getLayoutY() - 4);
+        setStyle("-fx-background-color: transparent; -fx-background-image: url('button_pressed2.png'); -fx-background-repeat: stretch; -fx-background-size: 190 70;");
     }
 
     public void initButtonListeners() {
@@ -51,21 +50,7 @@ public class SceneButtons extends Button {
             }
         });
 
-        setOnMouseEntered(new EventHandler<MouseEvent>() {
-            @Override
-            public void handle(MouseEvent mouseEvent) {
-                DropShadow dshadow = new DropShadow();
-                dshadow.setColor(Color.WHITE);
-                setEffect(dshadow);
-            }
-        });
 
-        setOnMouseExited(new EventHandler<MouseEvent>() {
-            @Override
-            public void handle(MouseEvent mouseEvent) {
-                setEffect(null);
-            }
-        });
     }
 
 
