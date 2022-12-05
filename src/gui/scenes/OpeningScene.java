658c3eec2e1936f79e756dec47dd7ff6853a3fc7
ikkai-r <rm.sales92@gmail.com>
2022-12-05 16:02:33 +0800

    Finished Choose Character. Make farm tiles next, shop, and inventory

--- src/gui/scenes/OpeningScene.java
+++ src/gui/scenes/OpeningScene.java
@@ -8,6 +8,7 @@ import javafx.event.EventHandler;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
@@ -41,8 +42,8 @@ public class OpeningScene extends GUI {
                 PlayerSubScene playerSubScene = new PlayerSubScene();
                 mainPane.getChildren().add(playerSubScene);
                 playerSubScene.moveSubScene();
-
-                promptPlayerNameChoice(playerSubScene);
+                playerSubScene.getPane().getStylesheets().add(this.getClass().getResource("/style.css").toExternalForm());
+                playerSelect(playerSubScene);
 
             }
         });
@@ -66,26 +67,28 @@ public class OpeningScene extends GUI {
         return new VBox(50, imageView, hbox);
     }
 
-    public void promptPlayerNameChoice(PlayerSubScene playerSubScene) {
+    public void playerSelect(PlayerSubScene playerSubScene) {
         SceneHeaderTxts characterTxt = new SceneHeaderTxts("Choose your character!");
         playerSubScene.getPane().getChildren().add(characterTxt);
         characterTxt.prefWidthProperty().bind(playerSubScene.widthProperty());
-        selectCharacter(playerSubScene);
+        HBox characterHBox = selectCharacter(playerSubScene);
+        HBox nameHBox = promptPlayerName(playerSubScene, characterTxt);
+        VBox vbox = new VBox(-110, characterHBox, nameHBox);
+        playerSubScene.getPane().getChildren().addAll(vbox);
     }
 
-    public void selectCharacter(PlayerSubScene playerSubScene) {
+    public HBox selectCharacter(PlayerSubScene playerSubScene) {
         Label label = new Label();
         label.setStyle("-fx-font-size: 40");
+
         Button boyCharacter = new Button();
         boyCharacter.setText("Boy");
-        boyCharacter.setStyle("-fx-text-fill: transparent; -fx-background-color: transparent; -fx-background-image: url('farmerboy.png'); -fx-background-repeat: stretch; -fx-background-size: 120 230;");
-        boyCharacter.setPrefWidth(120);
-        boyCharacter.setPrefHeight(230);
+        boyCharacter.setId("boy-button");
+        boyCharacter.setPrefSize(120, 230);
 
         Button girlCharacter = new Button();
-        girlCharacter.setStyle("-fx-text-fill: transparent; -fx-background-color: transparent; -fx-background-image: url('farmergirl.png'); -fx-background-repeat: stretch; -fx-background-size: 120 230;");
-        girlCharacter.setPrefWidth(120);
-        girlCharacter.setPrefHeight(230);
+        girlCharacter.setId("girl-button");
+        girlCharacter.setPrefSize(120, 230);
         girlCharacter.setText("Girl");
         girlCharacter.setOnAction(new EventHandler<ActionEvent>() {
 
@@ -93,7 +96,6 @@ public class OpeningScene extends GUI {
             public void handle(ActionEvent event) {
                 farmingGame.getFarmer().setFarmerCharacter(girlCharacter.getText());
                 label.setText("You are a " + girlCharacter.getText());
-                System.out.println(farmingGame.getFarmer().getFarmerCharacter());
             }
         });
 
@@ -103,7 +105,6 @@ public class OpeningScene extends GUI {
             public void handle(ActionEvent event) {
                 farmingGame.getFarmer().setFarmerCharacter(boyCharacter.getText());
                 label.setText("You are a " + boyCharacter.getText());
-                System.out.println(farmingGame.getFarmer().getFarmerCharacter());
             }
         });
 
@@ -112,7 +113,47 @@ public class OpeningScene extends GUI {
         hbox.setAlignment(Pos.CENTER);
         hbox.prefWidthProperty().bind(playerSubScene.widthProperty());
         hbox.prefHeightProperty().bind(playerSubScene.heightProperty());
-        playerSubScene.getPane().getChildren().add(hbox);
+
+        return hbox;
+    }
+
+    public HBox promptPlayerName(PlayerSubScene playerSubScene, SceneHeaderTxts characterTxt) {
+        Label label = new Label();
+        label.setStyle("-fx-font-size: 50");
+        label.setText("Name: ");
+        TextField nameField = new TextField();
+        nameField.setPrefHeight(70);
+        nameField.setId("name-field");
+        SceneButtons startGameBtn = new SceneButtons("Go!");
+        HBox hbox = new HBox(30);
+        hbox.getChildren().addAll(label, nameField, startGameBtn);
+        hbox.setAlignment(Pos.TOP_CENTER);
+        hbox.prefWidthProperty().bind(playerSubScene.widthProperty());
+        hbox.prefHeightProperty().bind(playerSubScene.heightProperty().divide(2).add(220));
+
+        startGameBtn.setOnAction(new EventHandler<ActionEvent>() {
+
+            @Override
+            public void handle(ActionEvent event) {
+                farmingGame.getFarmer().setFarmerName(nameField.getText());
+                if (farmingGame.getFarmer().getFarmerName() == null ||
+                        farmingGame.getFarmer().getFarmerName() == "" || farmingGame.getFarmer().getFarmerCharacter() == null) {
+                    characterTxt.setText("Choose a character and a name first!");
+                } else {
+                    farmingGame.getFarmer().setFarmerName(nameField.getText());
+                    FarmScene fs = new FarmScene();
+                    try {
+                        System.out.println("opened");
+                        fs.createFarm(OpeningScene.super.getMainStage());
+                    } catch (IOException e) {
+                        System.out.println("but failed");
+                        throw new RuntimeException(e);
+                    }
+                }
+            }
+        });
+
+        return hbox;
     }
 
 }
